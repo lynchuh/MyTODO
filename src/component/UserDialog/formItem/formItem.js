f7126a79e24b9f0d12199484c9e712243d51c7c8
lynchuh <lynchuh@gmail.com>
2018-08-20 15:58:02 +0800

    完成dialog基本功能,准备加入注册登陆功能

--- src/component/UserDialog/formItem/formItem.js
+++ src/component/UserDialog/formItem/formItem.js
@@ -1,16 +1,27 @@
 import React from 'react'
+import './formItem.css'
 
 export default class formItem extends React.Component{
   constructor(props){
     super(props)
+    this.state={
+      labelStatus:false
+    }
   }
 
   render(){
-
+    console.log(this.state)
     return (
-      <div className="formItem" >
-        <label htmlFor={this.props.formItem.id}>{this.props.formItem.word}</label>
-        <input type={this.props.formItem.type} name={this.props.formItem.id} value={this.props.value[this.props.formItem.id]} onChange={this.handleChange.bind(this,this.props.formItem.id)} />
+      <div className={!!this.state.labelStatus ?'formItem active':'formItem' } >
+        <label htmlFor={this.props.formItem.id}>{this.props.formItem.word} </label>
+        <input 
+        type={this.props.formItem.type} 
+        name={this.props.formItem.id} 
+        value={this.props.value[this.props.formItem.id]} 
+        onChange={this.handleChange.bind(this,this.props.formItem.id)}
+        onBlur={this.handleBlur.bind(this)}
+        onFocus={this.handleFocus.bind(this)}
+        />
       </div>
     )
   }
@@ -18,4 +29,22 @@ export default class formItem extends React.Component{
   handleChange(keyword,event){
     !!this.props.onChange && this.props.onChange.call(null,keyword,event)
   }
+  handleBlur(event){
+    if(!event.target.value){
+      this.setState({ //失焦且没有内容状态
+        labelStatus: false
+      })
+    }else{
+      this.setState({ //失焦且有内容
+        labelStatus: true
+      })
+    }
+  }
+  handleFocus(event){
+    console.log(event)
+    this.setState({ //聚焦状态
+      labelStatus: true
+    })
+  }
+  
 }
